d73335976989d0a01af3ac3236268d3539c17c82
Thani Suchoknand <tsucho@gmail.com>
2012-05-07 22:20:10 -0700

    Revert "Revert "pulled in master""
    This reverts commit fb03c3e950a1e05826c67d279c8391d9d95f2ac7.

---
+++ app/assets/javascripts/soundmanagersetup.js
@@ -0,0 +1,12 @@
+
+soundManager.url = '/assets/swf/';
+soundManager.flashVersion = 9; // optional: shiny features (default = 8)
+soundManager.useFlashBlock = false; // optionally, enable when you're ready to dive in
+/*
+ * read up on HTML5 audio support, if you're feeling adventurous.
+ * iPad/iPhone and devices without flash installed will always attempt to use it.
+*/
+soundManager.onready(function() {
+  // Ready to use; soundManager.createSound() etc. can now be called.
+  console.log('on');
+});
