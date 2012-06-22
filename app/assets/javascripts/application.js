58e04eae5cfd03dc7165b89fb3450a354eddd082
Thani Suchoknand <tsucho@gmail.com>
2012-06-22 03:09:02 -0700

    initial backbone nonsense

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/atndr
 //= require_tree .
 //= require soundmanager/soundmanager2
 //= require soundmanager/soundmanagersetup
