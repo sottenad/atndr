999ad0e38e499a4fbbd3c86184a99ce33d28fa28
Thani Suchoknand <tsucho@gmail.com>
2012-07-03 01:16:06 -0700

    got backbone working but all the models/views/... everything are in one file -- need to break it out somehow

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,8 @@ Atndr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'map#index'
+  # root :to => 'map#index'
+  root :to => 'x#index'
 
   # See how all your routes lay out with "rake routes"
 
